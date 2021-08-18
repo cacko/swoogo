719e2f6665134f0b77ddec3f4b234535abb4173a
cacko <alex@cacko.net>
2021-08-18 22:13:56 +0100

    Please our Lord and Savior the Great Linter.

--- app/auth.py
+++ app/auth.py
@@ -1,30 +1,27 @@
 from flask import request, abort, current_app
 import pyotp
 from functools import wraps
-from flask import (
-    Blueprint, request
-)
+from flask import Blueprint, request
 from werkzeug.exceptions import abort
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 
-bp = Blueprint('auth', __name__, url_prefix="/auth")
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 Auth = HTTPBasicAuth()
 
 HEADER_TOKEN = "X-Token"
 SETTING_TOTP_SECRET = "TOTP_SECRET"
-PASSWORD_CACKO = "PASSWORD_CACKO"
 
+users = {"cacko": "PASSWORD_CACKO"}
 
-users = {
-    "cacko": generate_password_hash(current_app.config.get(PASSWORD_CACKO)),
-}
 
 @Auth.verify_password
 def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
+    if username in users and check_password_hash(
+        generate_password_hash(current_app.config.get(users.get(username))), password
+    ):
         return username
+
 
 def auth_required(view):
     @wraps(view)
